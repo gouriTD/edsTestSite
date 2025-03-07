8d33f61d2f77b214765965dfeefe093e36218e3e
gouriTD <gouri30@gmail.com>
2025-03-07 20:09:24 +0530

    Adding a button on banner and providing its text from placeholders

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,4 +1,6 @@
-function decorateBanner(el) {
+import { fetchPlaceholders } from "../../scripts/aem.js"
+async function decorateBanner(el) {
+    const { clickHere } = await fetchPlaceholders('en');
     const bannerContent = el.querySelectorAll(':scope > div:nth-child(odd)');
     
     bannerContent.forEach((item, index) => {
@@ -8,6 +10,13 @@ function decorateBanner(el) {
       item.querySelector(':scope > div:nth-child(odd)').classList.add('banner-picture');
       // Add a class to the content
       item.querySelector(':scope > div:nth-child(even)').classList.add('banner-content');
+
+      // Add a dummy button with click me value
+      const bContent = document.querySelector('.banner-content');
+      const buttonEl = document.createElement('a');
+      buttonEl.setAttribute('href','/safari-details');
+      buttonEl.innerHTML = `<button>${clickHere}</button>`;
+      bContent.append(buttonEl);
     });
   }
   
